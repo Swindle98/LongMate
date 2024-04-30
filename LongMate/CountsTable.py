1332663ecdb24cc38576ae59e603a2b1d7bedd5a
Sam Windle <sam@windle.me.uk>
2024-04-30 13:30:36 +0100

    added the ability to generate the counts table from counts time points and groups.

--- LongMate/CountsTable.py
+++ LongMate/CountsTable.py
@@ -4,11 +4,71 @@ class CountsTable:
     """
     A top level class encompassing the manipulation of count tables and analysis functions.
     """
-    # Attributes
+        # Constructor
 
+    def __init__(self, df, timepoints, groups):
+
+        self.counts_type_check(df)
+        self.counts = df
+        self.import_timepoints(timepoints)
+        self.import_groups(groups)
+
+
+
+    # Attributes
+    
 
 
     # Methods
+    def import_groups(self, groups):
+        """
+        Import the groups for the analysis from a list or dictionary. The groups are added as a row on the dataframe and the dictionary is assigned to the groups attribute.
+        """
+        if not isinstance(groups, [list, dict]):
+            raise TypeError("The groups must be supllioed as a list or dictionary.")
+        
+        if isinstance(groups, list):
+            """
+             If the groups are supplied as a list, then the groups are assigned to the samples in the order they are presented.
+             a dictionary is then generated and assigned to the groups attribute.
+            """
+            self.counts['groups'] = groups
+            self.groups = {i: groups[i] for i in range(0, len(groups))}
+
+        if isinstance(groups, dict):
+            """
+            If the groups are supplied as a dictionary, then the groups are assigned to the samples based on the species.
+            The dictionary is then assigned to the groups attribute.
+            """
+            self.groups = groups
+            for key in groups.keys():
+                self.counts.loc[self.counts['species'] == key, 'groups'] = groups[key]
+
+
+    def import_timepoints(self, timepoints):
+        """
+        Import the timepoints for the analysis from a list or dictionary. The timepoints are added as a row on the dataframe and the dictionary is assigned to the timepoints attribute.
+        """
+        if not isinstance(timepoints, [list, dict]):
+            raise TypeError("The timepoints must be supplied as a list or dictionary.")
+        
+        if isinstance(timepoints, list):
+            """
+            If the timepoints are supplied as a list, then the timepoints are assigned to the samples in the order they are presented.
+            a dictionary is then generated and assigned to the timepoints attribute.
+            """
+            self.counts['timepoints'] = timepoints
+            self.timepoints = {i: timepoints[i] for i in range(0, len(timepoints))}
+
+        if isinstance(timepoints, dict):
+            """
+            If the timepoints are supplied as a dictionary, then the timepoints are assigned to the samples based on the species.
+            The dictionary is then assigned to the timepoints attribute.
+            """
+            self.timepoints = timepoints
+            for key in timepoints.keys():
+                self.counts.loc[self.counts['species'] == key, 'timepoints'] = timepoints[key]
+            
 
     def counts_type_check(self):
         """
@@ -18,9 +78,4 @@ class CountsTable:
             raise TypeError("The counts table must be a pandas DataFrame.")
         
 
-    # Constructor
 
-    def __init__(self, df):
-
-        counts_type_check(df)
-        self.counts = df
